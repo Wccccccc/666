b591ff457f5d3f2481c73f92d15ba4225295b60e
Wccccccc <web78998770@gmail.com>
2017-09-18 15:27:56 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args)  {
+         System.out.println("學號:106022005");
+        System.out.println("系級:行動商務與多媒體應用學系一年級");
+        System.out.println("姓名:陳韋岑");
+        System.out.println("時間:2017/09/18");
+        System.out.println("地點:\"I319\"");
+
+    }
+}
